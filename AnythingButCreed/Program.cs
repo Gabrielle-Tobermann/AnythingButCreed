7dfc10876c108a0efd783cff14b6cad2f7ce62bc
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-08-07 10:27:48 -0500

    Add project files.

---
+++ AnythingButCreed/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allSongs = new List<Song>();
+            var goodSongs = new List<Song>();
+
+            allSongs.Add(new Song("Blackbird", "The Beatles"));
+            allSongs.Add(new Song("CreedSong1", "Creed"));
+            allSongs.Add(new Song("CreedSong2", "Creed"));
+            allSongs.Add(new Song("CreedSong3", "Creed"));
+            allSongs.Add(new Song("Cinnamon Tree", "Esperanza Spalding"));
+            allSongs.Add(new Song("Radio Song", "Esperanza Spalding"));
+            allSongs.Add(new Song("Paradise Lost", "Outer Park"));
+            allSongs.Add(new Song("CreedSong4", "Creed"));
+            allSongs.Add(new Song("Bikes", "Rubblebucket"));
+            allSongs.Add(new Song("Poncianna", "Ahmad Jamal"));
+
+            var notCreed = allSongs.Where(num => num.Artist != "Creed");
+
+            goodSongs.AddRange(notCreed);
+
+            foreach(var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist}: {song.Name}");
+            }
+        }
+    }
+}
---
+++ AnythingButCreed/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnythingButCreed
+{
+    class Song
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+
+        public Song(string name, string artist)
+        {
+            Name = name;
+            Artist = artist;
+        }
+
+
+    }
+}
